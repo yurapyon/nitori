57809d0e1abf790a7427840908bd5739dfcd77ee
extrasharp <genericpb@gmail.com>
2020-08-30 12:52:31 -0500

    hunks

--- src/hunk.zig
+++ src/hunk.zig
@@ -1,43 +1,49 @@
-const std = @import("std");
+// based off of 'https://github.com/dbandstra/zig-hunk'
 
+const std = @import("std");
+const builtin = @import("builtin");
+
+const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 
-const interface = @import("interface.zig");
+const vtable = @import("vtable.zig");
 
 //;
 
 const HunkSide = struct {
     const Self = @This();
 
-    const Interface = struct {
-        const Impl = @Type(.Opaque);
-
-        alloc: fn (*Impl, usize, u29) Allocator.Error![]u8,
-        getMark: fn (*Impl) usize,
-        freeToMark: fn (*Impl, usize) void,
+    const VTable = struct {
+        alloc: fn (*HunkSide, usize, u29) Allocator.Error![]u8,
+        deinit_memory: fn (*HunkSide, usize) void,
     };
 
-    interface: *const Interface,
-    impl: *Interface.Impl,
+    vtable: *const VTable,
     allocator: Allocator,
+    mark: usize,
 
-    pub fn init(hunk_side: anytype) Self {
+    pub fn init(comptime SideType: type) Self {
         return .{
-            .interface = comptime interface.populate(Interface, @TypeOf(hunk_side).Child),
-            .impl = @ptrCast(*Interface.Impl, hunk_side),
+            .vtable = comptime vtable.populate(VTable, SideType),
             .allocator = .{
                 .allocFn = allocFn,
                 .resizeFn = resizeFn,
             },
+            .mark = 0,
         };
     }
 
     pub fn getMark(self: *Self) usize {
-        return self.interface.getMark(self.impl);
+        return self.mark;
     }
 
     pub fn freeToMark(self: *Self, mark: usize) void {
-        return self.interface.freeToMark(self.impl, mark);
+        assert(mark <= self.mark);
+        if (mark == self.mark) return;
+        if (builtin.mode == builtin.Mode.Debug) {
+            self.vtable.deinit_memory(self, mark);
+        }
+        self.mark = mark;
     }
 
     fn allocFn(
@@ -51,7 +57,7 @@ const HunkSide = struct {
         const real_len = if (len_align == 0) len else blk: {
             break :blk std.mem.alignBackwardAnyAlign(len, len_align);
         };
-        return self.interface.alloc(self.impl, real_len, ptr_align);
+        return self.vtable.alloc(self, real_len, ptr_align);
     }
 
     // in place resize
@@ -67,6 +73,7 @@ const HunkSide = struct {
         if (new_len == 0) {
             // free memory
             // in this case its a no-op and leaks memory
+            return 0;
         } else if (new_len <= buf.len) {
             // TODO is this right to use?
             return std.mem.alignAllocLen(buf.len, new_len, len_align);
@@ -81,4 +88,99 @@ const HunkSide = struct {
 
 const Hunk = struct {
     const Self = @This();
+
+    const Low = struct {
+        pub fn alloc(side: *HunkSide, len: usize, ptr_align: u29) Allocator.Error![]u8 {
+            const hunk = @fieldParentPtr(Hunk, "low", side);
+
+            const buf_start = @ptrToInt(hunk.buffer.ptr);
+            const adj_idx = std.mem.alignForward(buf_start + hunk.low.mark, ptr_align) - buf_start;
+            const next_mark = adj_idx + len;
+            if (next_mark > hunk.buffer.len - hunk.high.mark) {
+                return error.OutOfMemory;
+            }
+            const ret = hunk.buffer[adj_idx..next_mark];
+            hunk.low.mark += len;
+            return ret;
+        }
+
+        pub fn deinit_memory(side: *HunkSide, mark: usize) void {
+            const hunk = @fieldParentPtr(Hunk, "low", side);
+            std.mem.set(u8, hunk.buffer[mark..side.mark], undefined);
+        }
+    };
+
+    const High = struct {
+        pub fn alloc(side: *HunkSide, len: usize, ptr_align: u29) Allocator.Error![]u8 {
+            const hunk = @fieldParentPtr(Hunk, "high", side);
+
+            const buf_start = @ptrToInt(hunk.buffer.ptr);
+            const buf_end = buf_start + hunk.buffer.len;
+            const adj_idx = std.mem.alignBackward(buf_end - hunk.high.mark, ptr_align) - buf_start;
+            const next_mark = adj_idx - len;
+            if (next_mark < hunk.low.mark) {
+                return error.OutOfMemory;
+            }
+            const ret = hunk.buffer[next_mark..adj_idx];
+            hunk.high.mark += len;
+            return ret;
+        }
+
+        pub fn deinit_memory(side: *HunkSide, mark: usize) void {
+            const hunk = @fieldParentPtr(Hunk, "high", side);
+            const start = hunk.buffer.len - side.mark;
+            const end = hunk.buffer.len - mark;
+            std.mem.set(u8, hunk.buffer[start..end], undefined);
+        }
+    };
+
+    buffer: []u8,
+    low: HunkSide,
+    high: HunkSide,
+
+    pub fn init(buffer: []u8) Self {
+        return .{
+            .buffer = buffer,
+            .low = HunkSide.init(Low),
+            .high = HunkSide.init(High),
+        };
+    }
 };
+
+// tests ===
+
+const testing = std.testing;
+
+test "hunk" {
+    // test a few random operations. very low coverage. write more later
+    var buf: [100]u8 = undefined;
+    var hunk = Hunk.init(buf[0..]);
+
+    const high_mark = hunk.high.getMark();
+
+    _ = try hunk.low.allocator.alloc(u8, 7);
+    _ = try hunk.high.allocator.alloc(u8, 8);
+
+    testing.expectEqual(@as(usize, 7), hunk.low.mark);
+    testing.expectEqual(@as(usize, 8), hunk.high.mark);
+
+    _ = try hunk.high.allocator.alloc(u8, 8);
+
+    testing.expectEqual(@as(usize, 16), hunk.high.mark);
+
+    const low_mark = hunk.low.getMark();
+
+    _ = try hunk.low.allocator.alloc(u8, 100 - 7 - 16);
+
+    testing.expectEqual(@as(usize, 100 - 16), hunk.low.mark);
+
+    testing.expectError(error.OutOfMemory, hunk.high.allocator.alloc(u8, 1));
+
+    hunk.low.freeToMark(low_mark);
+
+    _ = try hunk.high.allocator.alloc(u8, 1);
+
+    hunk.high.freeToMark(high_mark);
+
+    testing.expectEqual(@as(usize, 0), hunk.high.mark);
+}
---
+++ src/vtable.zig
@@ -0,0 +1,168 @@
+// based off of 'https://github.com/Hejsil/zig-interface'
+
+// no restriction on params
+//   vtable fns dont have to be methods
+//   can work with ZSTs
+// optional fns
+
+// const VTable = struct {
+//     fn1: fn () void,
+//     fn2: fn () void,
+// };
+//
+// // input should be
+// const In = struct {
+//     fn fn1() void {}
+//     fn fn2() void {}
+// };
+
+const std = @import("std");
+const builtin = @import("builtin");
+const TypeInfo = builtin.TypeInfo;
+
+const assert = std.debug.assert;
+
+//;
+
+fn checkCompatibility(
+    comptime VTable: type,
+    comptime T: type,
+    comptime expect: TypeInfo.Fn,
+    comptime actual: TypeInfo.Fn,
+) void {
+    assert(!expect.is_generic);
+    assert(!expect.is_var_args);
+
+    assert(expect.calling_convention == actual.calling_convention);
+    assert(expect.is_generic == actual.is_generic);
+    assert(expect.is_var_args == actual.is_var_args);
+    assert(expect.return_type.? == actual.return_type.?);
+    assert(expect.args.len == actual.args.len);
+
+    for (expect.args) |expect_arg, i| {
+        const actual_arg = actual.args[i];
+        assert(!expect_arg.is_generic);
+        assert(expect_arg.is_generic == actual_arg.is_generic);
+        assert(expect_arg.is_noalias == actual_arg.is_noalias);
+
+        const expect_arg_info = @typeInfo(expect_arg.arg_type.?);
+        if (i == 0 and
+            expect_arg_info == .Pointer and
+            @hasDecl(VTable, "Impl") and
+            expect_arg_info.Pointer.child == VTable.Impl)
+        {
+            const expect_ptr = expect_arg_info.Pointer;
+            const actual_ptr = @typeInfo(actual_arg.arg_type.?).Pointer;
+            assert(expect_ptr.size == TypeInfo.Pointer.Size.One);
+            assert(actual_ptr.child == T);
+            assert(expect_ptr.size == actual_ptr.size);
+            assert(expect_ptr.is_const == actual_ptr.is_const);
+            assert(expect_ptr.is_volatile == actual_ptr.is_volatile);
+        } else {
+            assert(expect_arg.arg_type.? == actual_arg.arg_type.?);
+        }
+    }
+}
+
+pub fn populate(comptime VTable: type, comptime T: type) *const VTable {
+    const Global = struct {
+        const vtable = blk: {
+            var ret: VTable = undefined;
+            inline for (@typeInfo(VTable).Struct.fields) |field_info| {
+                // currently, accessing default values is a compiler bug
+                //   see issue 'https://github.com/ziglang/zig/issues/5508'
+                //   for now, use optionals instead
+                const FieldType = field_info.field_type;
+                switch (@typeInfo(FieldType)) {
+                    .Fn => |expect| {
+                        const field = @field(T, field_info.name);
+                        const actual = @typeInfo(@TypeOf(field)).Fn;
+                        checkCompatibility(VTable, T, expect, actual);
+                        @field(ret, field_info.name) = @ptrCast(FieldType, field);
+                    },
+                    .Optional => |opt| {
+                        const FnType = opt.child;
+                        const expect = @typeInfo(FnType).Fn;
+
+                        var found_impl_decl: ?TypeInfo.Declaration = null;
+                        inline for (@typeInfo(T).Struct.decls) |impl_decl| {
+                            if (std.mem.eql(u8, field_info.name, impl_decl.name)) {
+                                found_impl_decl = impl_decl;
+                            }
+                        }
+
+                        const field = if (found_impl_decl) |found|
+                            @field(T, found.name)
+                        else
+                            @field(VTable, field_info.name);
+
+                        const actual = @typeInfo(@TypeOf(field)).Fn;
+                        checkCompatibility(VTable, T, expect, actual);
+                        @field(ret, field_info.name) = @ptrCast(FnType, field);
+                    },
+                    else => {
+                        assert(false);
+                    },
+                }
+            }
+            break :blk ret;
+        };
+    };
+    return &Global.vtable;
+}
+
+// tests ===
+
+const Obj = struct {
+    const Self = @This();
+
+    const VTable = struct {
+        doSomething1: fn () void,
+        doSomething2: ?fn (usize) usize,
+
+        fn doSomething2(in: usize) usize {
+            std.log.warn("default do something 2: {}\n", .{in + 2});
+            return in + 2;
+        }
+    };
+
+    vtable: *const VTable,
+
+    fn init(obj: anytype) Obj {
+        return .{
+            .vtable = comptime populate(VTable, @TypeOf(obj).Child),
+        };
+    }
+
+    fn doSomething1(self: *Self) void {
+        self.vtable.doSomething1();
+    }
+
+    fn doSomething2(self: *Self, in: usize) usize {
+        return self.vtable.doSomething2.?(in);
+    }
+};
+
+test "interface" {
+    var o1 = Obj.init(&struct {
+        fn doSomething1() void {
+            std.log.warn("o1 do something 1\n", .{});
+        }
+
+        fn doSomething2(in: usize) usize {
+            std.log.warn("o1 do something 2: {}\n", .{in + 3});
+            return in + 3;
+        }
+    }{});
+
+    var o2 = Obj.init(&struct {
+        fn doSomething1() void {
+            std.log.warn("o2 do something 1\n", .{});
+        }
+    }{});
+
+    o1.doSomething1();
+    _ = o1.doSomething2(10);
+    o2.doSomething1();
+    _ = o2.doSomething2(10);
+}
